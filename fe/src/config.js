794f331f585a8110756e99c45acb34bc55e47a2a
Kartikeya Gupta <kartikeya.229302253@muj.manipal.edu>
2025-04-11 02:42:12 +0530

    BACKEND_URL fix for productions

--- fe/src/config.js
+++ fe/src/config.js
@@ -1,1 +1,1 @@
-export const BACKEND_URL = import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "/api";
+export const BACKEND_URL = import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/" : "/api";
